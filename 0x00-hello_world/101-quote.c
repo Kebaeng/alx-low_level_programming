021c8113d6e804316dac3ebe54e4d42301b88d8d
Kebaeng <Mkebaeng09@gmail.com>
2023-06-15 17:42:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 #include <unstd.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - A c program that prints a line in the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
